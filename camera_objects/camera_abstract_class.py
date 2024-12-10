2ee775093fe0515cc3357be21dcaf7f11a33807b
張議隆 <nick900828@gmail.com>
2024-12-10 13:01:13 +0800

    Add full docstring

--- camera_objects/camera_abstract_class.py
+++ camera_objects/camera_abstract_class.py
@@ -3,26 +3,94 @@ from typing import Tuple
 
 import numpy as np
 
-class Two_Cameras_System(ABC):
-    def __init__(self):
-        super().__init__()
+class TwoCamerasSystem(ABC):
+    """
+    Abstract class for two cameras system.
 
+    Functions:
+        __init__() -> None
+        get_grayscale_images() -> Tuple[bool, np.ndarray, np.ndarray]
+        get_depth_image() -> Tuple[bool, np.ndarray]
+        get_width() -> int
+        get_height() -> int
+        release() -> bool
+    """
+    def __init__(self) -> None:
+        """
+        Initialize two cameras system.
+
+        args:
+        No arguments.
+
+        returns:
+        No return.
+        """
     @abstractmethod
     def get_grayscale_images(self) -> Tuple[bool, np.ndarray, np.ndarray]:
+        """
+        Get grayscale images for both camera.
+
+        args:
+        No arguments.
+        
+        returns:
+        Tuple[bool, np.ndarray, np.ndarray]:
+            - bool: Whether images grabbing is successful or not.
+            - np.ndarray: left grayscale image.
+            - np.ndarray: right grayscale image.
+        """
         return
-    
     @abstractmethod
     def get_depth_image(self) -> Tuple[bool, np.ndarray]:
+        """
+        Get depth images for the camera system.
+        
+        args:
+        No arguments.
+        
+        returns:
+        Tuple[bool, np.ndarray]:
+            - bool: Whether depth image grabbing is successful or not.
+            - np.ndarray: depth grayscale image.
+        """
         return
-    
     @abstractmethod
     def get_width(self) -> int:
-        return
-    
+        """
+        Get width for the camera system.
+        
+        args:
+        No arguments.
+        
+        returns:
+        int:
+            - int: Width of the camera system.
+        """
+        return    
     @abstractmethod
     def get_height(self) -> int:
+        """
+        Get height for the camera system.
+        
+        args:
+        No arguments.
+        
+        returns:
+        int:
+            - int: Height of the camera system.
+        """
         return
-    
     @abstractmethod
     def release(self) -> bool:
+        """
+        Release the camera system.
+        
+        args:
+        No arguments.
+        
+        returns:
+        bool:
+            - bool: Whether releasing is successful or not.
+        """
         return
+    
