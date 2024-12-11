c2068b59734e932d928aa441f4ca15ccf6e01eca
張議隆 <nick900828@gmail.com>
2024-12-11 14:45:07 +0800

    Minor fix (#21)
    * Fix minor errors
    
    Uploaded untested code last PR
    
    * Include width and height as argument
    
    * Remove Pyqt5 from required packages
    
    * Remove trailing white spaces

--- main_realsense.py
+++ main_realsense.py
@@ -5,19 +5,21 @@ from ui_objects.opencv_ui_controller import OpencvUIController
 from camera_objects.realsense_camera_system import RealsenseCameraSystem
 
 if __name__ == "__main__":
-    # Camera intrinsic parameters, should be loaded somewhere else?
     # D415
     FOCAL_LENGTH = 908.36  # in pixels
     BASELINE = 55  # in mm
+    WIDTH = 1280
+    HEIGHT = 720
 
-    # Camera intrinsic parameters, should be loaded somewhere else?
     # D435
     # FOCAL_LENGTH = 425.203  # in pixels
     # BASELINE = 50  # in mm
+    # WIDTH = 848
+    # HEIGHT = 480
 
     UI = OpencvUIController(system_prefix="Realsense", focal_length=FOCAL_LENGTH, baseline=BASELINE)
 
-    cameras = RealsenseCameraSystem()
+    cameras = RealsenseCameraSystem(width = WIDTH, height = HEIGHT)
     UI.set_camera_system(cameras)
 
     UI.start()
