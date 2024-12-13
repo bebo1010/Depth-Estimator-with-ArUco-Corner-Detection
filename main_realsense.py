4cc44eca6cfbb7766e23ac615647ae3ce3f142c7
張議隆 <nick900828@gmail.com>
2024-12-13 16:26:08 +0800

    Add config lines back

--- main_realsense.py
+++ main_realsense.py
@@ -11,6 +11,12 @@ if __name__ == "__main__":
     WIDTH = 1280
     HEIGHT = 720
 
+    # D435
+    # FOCAL_LENGTH = 425.203  # in pixels
+    # BASELINE = 50  # in mm
+    # WIDTH = 848
+    # HEIGHT = 480
+
     UI = OpencvUIController(system_prefix="Realsense", focal_length=FOCAL_LENGTH, baseline=BASELINE)
 
     cameras = RealsenseCameraSystem(width = WIDTH, height = HEIGHT)
