2bb1a9d0a37de0013272aadd5c558f0dd44c097e
張議隆 <nick900828@gmail.com>
2024-12-20 11:09:44 +0800

    Include the tests for error handling
    Add error handling tests for parse_yaml_config()

--- unit_tests/test_file_utils.py
+++ unit_tests/test_file_utils.py
@@ -4,6 +4,9 @@ Unit tests for the file_utils module.
 
 import unittest
 from unittest.mock import patch, mock_open
+
+import yaml
+
 from utils.file_utils import get_starting_index, parse_yaml_config
 
 class TestFileUtils(unittest.TestCase):
@@ -42,6 +45,24 @@ class TestFileUtils(unittest.TestCase):
             config = parse_yaml_config("dummy_path")
             self.assertEqual(config['camera_settings']['width'], 1920)
             self.assertEqual(config['camera_settings']['height'], 1084)
+
+    @patch('builtins.open', side_effect=OSError("File not found"))
+    def test_parse_yaml_config_oserror(self, mock_open):
+        """
+        Test parsing of YAML configuration file when OSError is raised.
+        """
+        config = parse_yaml_config("dummy_path")
+        self.assertIsNone(config)
+
+    @patch('builtins.open')
+    def test_parse_yaml_config_yamlerror(self, mock_open):
+        """
+        Test parsing of YAML configuration file when YAMLError is raised.
+        """
+        mock_open.return_value = mock_open(read_data="invalid_yaml: [")
+        with patch('yaml.safe_load', side_effect=yaml.YAMLError("YAML error")):
+            config = parse_yaml_config("dummy_path")
+            self.assertIsNone(config)
 
 if __name__ == '__main__':
     unittest.main()
